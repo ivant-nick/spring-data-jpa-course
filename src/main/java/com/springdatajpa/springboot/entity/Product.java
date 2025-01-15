b554a310b0b836cd25375dc15f64bf91f7cb80b9
CodeCrafter <ivan.turchyn94@gmail.com>
2025-01-15 13:48:38 +0100

    Section 21. Spring Data JPA - Getting Started

---
+++ src/main/java/com/springdatajpa/springboot/entity/Product.java
@@ -0,0 +1,57 @@
+package com.springdatajpa.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(
+        name = "products",
+        schema = "ecommerce",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "sku_unique", columnNames = "sku")
+        }
+)
+public class Product {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY,
+            generator = "product_generator"
+    )
+
+    @SequenceGenerator(
+            name = "product_generator",
+            sequenceName = "product_sequence_name",
+            allocationSize = 1
+    )
+    private long id;
+
+    @Column(name = "stock_keeping_unit")
+    private String sku;
+
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private boolean active;
+    private String imageUrl;
+
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+
+}
